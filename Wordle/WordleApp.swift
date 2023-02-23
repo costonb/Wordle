f2e6057d9c852266abbb678c92675f4e18c80520
Brandon Coston <costonb@gmail.com>
2023-02-22 18:02:10 -0800

    Initial Commit

---
+++ Wordle/WordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleApp.swift
+//  Wordle
+//
+//  Created by Brandon Coston on 2/22/23.
+//
+
+import SwiftUI
+
+@main
+struct WordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
